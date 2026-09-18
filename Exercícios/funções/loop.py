7d227d3bbce7aa62ea028470da72ce020a723bfd
PAVANE MARGO POLONI RODRIGUES <76344568020@senacrs.edu.br>
2026-09-18 19:57:33 -0300

    mensagem

--- Exercícios/funções/loop.py
+++ Exercícios/funções/loop.py
@@ -3,5 +3,21 @@ def dobrar(numeros:[]):
         numero = numero * 2
         print(numero)
 
+def filtrar_pares(numeros: list):
+    pares = []
+    for numero in numeros:
+        if numero % 2 == 0:
+            pares.append(numero)
+
+    return pares        
+    
+
+
+
+
 if __name__=="__main__":
     dobrar([1,2,3,4,5])
+
+
+numeros_pares = filtrar_pares([1, 2, 3, 4, 5, 6])
+print(numeros_pares)
