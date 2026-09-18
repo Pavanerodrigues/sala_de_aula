1303ee4953bdec4b043bcbf7053fc3d1c23e0f10
PAVANE MARGO POLONI RODRIGUES <76344568020@senacrs.edu.br>
2026-09-17 21:56:16 -0300

    mensagem

--- Exercícios/funções/condicionais.py
+++ Exercícios/funções/condicionais.py
@@ -12,12 +12,29 @@ def verificar_maioridade(idade:int):
     if idade >= 18:
         return "maior de idade"
     else:
-        return "menor de idade"
-    
-       
+        return "menor de idada"
+
+def verificar_paridade(numero:int):
+    if numero % 2 == 0:
+        return "Par"
+    else:
+        return "Impar"
+
+def classificar_numero(numero:int):
+    if numero > 0:
+        return "Positivo"
+    if numero < 0:
+        return "Negativo"
+    return "Zero"
+
+def calcular_resultado(nota1:int, nota2:int):
+    if nota1 >= 7.0:
+        return "aprovado"
+    else:
+        return "reprovado"
 
 
-   
+
 
 
 
@@ -26,6 +43,15 @@ if __name__ =="__main__":
     print(teste) 
 
     idade = verificar_maioridade(18)
-    print(idade)
+    print(idade) 
 
+    Ímpar = verificar_paridade(7) 
+    print(Ímpar) 
+    Par = verificar_paridade(12)
+    print(Par) 
 
+    negativo = classificar_numero(-5)
+    print(f"3- {negativo}")  
+    
+    aprovado = calcular_resultado(8.0, 6.0)
+    print(f"4- {aprovado}")
